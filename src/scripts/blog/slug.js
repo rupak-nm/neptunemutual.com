e5fa3fb4c7f1f3f01f65b10dbb7bbe576e88fbd5
rupak-nm <rupak@neptunemutual.com>
2023-01-16 13:14:56 +0545

    Added language dropdown script to all page scripts

--- src/scripts/blog/slug.js
+++ src/scripts/blog/slug.js
@@ -2,6 +2,7 @@
 import '../components/nav/mega-menu'
 import '../components/nav/hamburger'
 import '../components/nav/mobile-nav-item'
+import '../components/language/index'
 import '../global/cookie'
 import '../global/video'
 import '../global/index'
