e0c5b4d121f20ea51a18d8a068d37acea006279f
flashburst <82953782+flashburst@users.noreply.github.com>
2023-01-17 13:54:29 +0530

    Added google analytics and microsoft clarity

---
+++ src/scripts/global/analytics/google-analytics.js
@@ -0,0 +1,28 @@
+function addGoogleAnalytics() {
+  const ID = "ga-script"
+
+  if (document.getElementById(ID)) {
+    return
+  }
+
+  // Global site tag (gtag.js) - Google Analytics
+  const gtagSource = `https://www.googletagmanager.com/gtag/js?id=${window.googleAnalyticsId}`;
+  const globalScript = document.createElement("script");
+  globalScript.async = true
+  globalScript.setAttribute("src", gtagSource);
+  document.head.appendChild(globalScript);
+
+  const googleAnalyticsScriptText = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      
+      gtag('config', '${window.googleAnalyticsId}');
+      `;
+  const script = document.createElement("script");
+  script.id = ID
+  script.textContent = googleAnalyticsScriptText
+  document.head.appendChild(script);
+}
+
+export { addGoogleAnalytics }
