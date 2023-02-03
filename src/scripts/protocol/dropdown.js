550e30f6d502bfebb7dd1a209a91bf049e7ef6dd
rupak-nm <rupak@neptunemutual.com>
2023-02-03 13:15:20 +0545

    Updated Network Dropdown in protocols page for mobile

--- src/scripts/protocol/dropdown.js
+++ src/scripts/protocol/dropdown.js
@@ -2,9 +2,16 @@
   const toggleButton = document.getElementById('NetworkSelectButton')
   const menu = document.querySelector('#NetworkSelectButton + .menu[data-open]')
 
-  toggleButton.addEventListener('click', () => {
+  toggleButton.addEventListener('click', (e) => {
+    e.stopPropagation()
+
     const currentState = menu.getAttribute('data-open')
     const newState = currentState === 'true' ? 'false' : 'true'
     menu.setAttribute('data-open', newState)
+
+    document.addEventListener('click', ev => {
+      const isClickOutside = !ev.target.closest("#NetworkSelectButton + .menu[data-open]")
+      if(isClickOutside) menu.setAttribute('data-open', 'false')
+    })
   })
 }
