0f179069545ab8a69558587f60d2e605dd9041de
rupak-nm <rupak@neptunemutual.com>
2023-02-16 09:55:55 +0545

    - Updated conflicting dimmer implementation for docs content-sidebar & search - Added close search modal on click outside - Show sidebar by default when no javascript available

--- src/scripts/docs/sidebar.js
+++ src/scripts/docs/sidebar.js
@@ -3,15 +3,21 @@
   const sidebar = document.querySelector('.documentation .ui.sidebar.menu[data-open]')
   const sidebarOverlay = document.querySelector('.documentation .dimmer.sidebar')
 
-  const toggleSidebarAndOverlay = () => {
+  sidebar.removeAttribute('data-noscript')
+
+  const toggleSidebarAndOverlay = (overlayClick = false) => {
     const currentState = sidebar.getAttribute('data-open')
     const newState = currentState === 'false' ? 'true' : 'false'
-    sidebar.setAttribute('data-open', newState)
+    sidebar.setAttribute('data-open', overlayClick ? 'false' : newState)
 
+    if (overlayClick) {
+      sidebarOverlay.classList.add('hidden')
+      return
+    }
     sidebarOverlay.classList.toggle('hidden')
   }
 
-  contentMenuButton.addEventListener('click', toggleSidebarAndOverlay)
+  contentMenuButton.addEventListener('click', () => toggleSidebarAndOverlay(false))
 
-  sidebarOverlay.addEventListener('click', toggleSidebarAndOverlay)
+  sidebarOverlay.addEventListener('click', () => toggleSidebarAndOverlay(true))
 }
