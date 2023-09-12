87c5f06e75236542a4cf65a99a21dab7ac2d655c
acharyarupak391 <acharyarupak391@gmail.com>
2023-09-12 18:04:51 +0545

    Moved networks.ts file to data

--- src/data/networks.ts
+++ src/data/networks.ts
@@ -10,10 +10,10 @@ const networks: NetworkOption[] = [
   {
     name: 'Ethereum',
     src: import(
-      '../../elements/icons/custom/Brands/default/SvgEthereum.svg?raw'
+      '../elements/icons/custom/Brands/default/SvgEthereum.svg?raw'
     ).then((x: any) => x.default),
     srcDark: import(
-      '../../elements/icons/custom/Brands/dark/SvgEthereumDark.svg?raw'
+      '../elements/icons/custom/Brands/dark/SvgEthereumDark.svg?raw'
     ).then((x: any) => x.default),
     text: 'ethereum.neptunemutual.net',
     link: 'https://ethereum.neptunemutual.net'
@@ -21,10 +21,10 @@ const networks: NetworkOption[] = [
   {
     name: 'Arbitrum',
     src: import(
-      '../../elements/icons/custom/Brands/default/SvgArbitrum.svg?raw'
+      '../elements/icons/custom/Brands/default/SvgArbitrum.svg?raw'
     ).then((x: any) => x.default),
     srcDark: import(
-      '../../elements/icons/custom/Brands/dark/SvgArbitrumDark.svg?raw'
+      '../elements/icons/custom/Brands/dark/SvgArbitrumDark.svg?raw'
     ).then((x: any) => x.default),
     text: 'arbitrum.neptunemutual.net',
     link: 'https://arbitrum.neptunemutual.net'
@@ -32,10 +32,10 @@ const networks: NetworkOption[] = [
   {
     name: 'BNB Smart Chain',
     src: import(
-      '../../elements/icons/custom/Brands/default/SvgBNBChain.svg?raw'
+      '../elements/icons/custom/Brands/default/SvgBNBChain.svg?raw'
     ).then((x: any) => x.default),
     srcDark: import(
-      '../../elements/icons/custom/Brands/dark/SvgBNBChainDark.svg?raw'
+      '../elements/icons/custom/Brands/dark/SvgBNBChainDark.svg?raw'
     ).then((x: any) => x.default),
     text: 'bsc.neptunemutual.net',
     link: 'https://bsc.neptunemutual.net'
@@ -60,14 +60,14 @@ const swaps: Array<{
   href: string
 }> = [
   {
-    src: import('../../elements/icons/custom/sushi-arb.svg?raw').then((x: any) => x.default),
-    srcDark: import('../../elements/icons/custom/sushi-arb-dark.svg?raw').then((x: any) => x.default),
+    src: import('../elements/icons/custom/sushi-arb.svg?raw').then((x: any) => x.default),
+    srcDark: import('../elements/icons/custom/sushi-arb-dark.svg?raw').then((x: any) => x.default),
     title: 'Sushiswap (Arbitrum)',
     href: `https://www.sushi.com/swap?token0=${tokens.arb.usdc}&token1=${tokens.arb.npm}&chainId=42161`
   },
   {
-    src: import('../../elements/icons/custom/uniswap-eth.svg?raw').then((x: any) => x.default),
-    srcDark: import('../../elements/icons/custom/uniswap-eth-dark.svg?raw').then((x: any) => x.default),
+    src: import('../elements/icons/custom/uniswap-eth.svg?raw').then((x: any) => x.default),
+    srcDark: import('../elements/icons/custom/uniswap-eth-dark.svg?raw').then((x: any) => x.default),
     title: 'Uniswap (Ethereum)',
     href: `https://app.uniswap.org/#/swap?inputCurrency=USDC&outputCurrency=${tokens.eth.npm}&chain=mainnet`
   }
